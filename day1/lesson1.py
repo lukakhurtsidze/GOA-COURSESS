9bf45689e3e80695e087edb60119e74467765f4f
lukakhurtsidze <lukinto2010@gmail.com>
2024-02-15 20:16:07 +0300

    first commit

---
+++ day1/lesson1.py
@@ -0,0 +1,5 @@
+name="luka"
+surname="khurtsidze"
+age=14
+hight=175.1
+print (" my name is "+ name + " I am " + str(age) + " my hight is " + str(hight))
---
+++ day2/lesson2.py
@@ -0,0 +1,3 @@
+print("I followed  low budget stories")
+print("l met and make  friends Jeko Tarieladze,Gigi Bendeliani,Nanuka Tughushi")
+print("In my opinion, Goa is a programming academy where they teach you not only programming, but also how to achieve your goals and prepare you for the future.")
